fe9ceffe16eeda95f9f4fe2c1e4aa2543decef30
giovanniurbano <urbagio@gmail.com>
2021-03-08 11:32:14 +0100

    parte 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -21,4 +21,30 @@ public class Libretto {
 		return s;
 	}
 	
+	public List<Voto> listaVotiUguali(int punteggio){
+		List<Voto> s = new ArrayList<Voto>();
+		for(Voto v : voti)
+			if(v.getVoto() == punteggio)
+				s.add(v);
+		return s;
+	}
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto s = new Libretto();
+		
+		for(Voto v : voti)
+			if(v.getVoto() == punteggio)
+				s.add(v);
+		return s;
+	}
+	
+	public Voto ricercaCorso(String nomeCorso) {
+		Voto s = null;
+		for(Voto v : voti)
+			if(v.getNome().compareTo(nomeCorso) == 0){
+				s = v;
+				break;
+			}
+		return s;
+	}
 }
